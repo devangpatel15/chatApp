7bb6f485e6247e358472acf0a3a20ac32837aba1
devangpatel15 <devang150803@gmail.com>
2025-05-16 12:28:00 +0530

    prepare for render development

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
+// const http = require("http");
+// const express = require("express");
+// const path = require("path");
+// const { Server } = require("socket.io");
+
+// const app = express();
+// const server = http.createServer(app);
+// const io = new Server(server);
+
+// // Socket.io
+
+// io.on("connection", (socket) => {
+//   //socket = client
+
+//   //   socket.on("user-message", (message) => {
+//   //     io.emit("message", message);
+//   //   });
+//   socket.on("user-message", (message) => {
+//     socket.broadcast.emit("message", message);
+//   });
+// });
+
+// app.use(express.static(path.resolve("./public")));
+
+// app.get("/", (req, res) => {
+//   return res.sendFile("/public/index.html");
+// });
+// const PORT = 9000;
+
+// server.listen(PORT, () => console.log(`server started at Port:${PORT}`));
+
 const http = require("http");
 const express = require("express");
 const path = require("path");
@@ -7,24 +38,21 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// Socket.io
+// Serve static files from "public"
+app.use(express.static(path.resolve("./public")));
 
+// Main route
+app.get("/", (req, res) => {
+  return res.sendFile(path.resolve("./public/index.html"));
+});
+
+// Socket.io setup
 io.on("connection", (socket) => {
-  //socket = client
-
-  //   socket.on("user-message", (message) => {
-  //     io.emit("message", message);
-  //   });
   socket.on("user-message", (message) => {
     socket.broadcast.emit("message", message);
   });
 });
 
-app.use(express.static(path.resolve("./public")));
-
-app.get("/", (req, res) => {
-  return res.sendFile("/public/index.html");
-});
-const PORT = 9000;
-
-server.listen(PORT, () => console.log(`server started at Port:${PORT}`));
+// Use environment PORT or default to 9000
+const PORT = process.env.PORT || 9000;
+server.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
